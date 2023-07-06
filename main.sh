639ea507cab3a1434b196e6fe277a38df1c49ab7
ferrreo <harderthanfire@gmail.com>
2023-07-06 18:15:28 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,6 +6,7 @@ DEBIAN_FRONTEND=noninteractive
 git clone https://github.com/waycrate/wayshot.git --recurse-submodules
 cd wayshot
 cp -rvf ../debian ./
+cp -vf ../Makefile ./Makefile
 
 # Get build deps
 apt-get build-dep ./ -y
